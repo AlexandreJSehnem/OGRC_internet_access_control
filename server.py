6699cb2e582d3588e01631ccfacbe51b3036a323
AlexandreJSehnem <alexandre.jsehnem@gmail.com>
2022-07-22 15:13:55 -0300

    showing the html file

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-import easy_get_set
+# import easy_get_set
 from tornado import httputil
 import tornado.ioloop
 import tornado.web
+import tornado.template
 # import tornado.httpclient as httpclient
 PORT = '1.3.6.1.2.1.2.2.1.7.'
 
@@ -9,19 +10,21 @@ PORT = '1.3.6.1.2.1.2.2.1.7.'
 class MainHandler(tornado.web.RequestHandler):
 
     def __init__(self, application, request: httputil.HTTPServerRequest, **kwargs) -> None:
-        self.snmp_handler = easy_get_set.snMPP()
+        # self.snmp_handler = easy_get_set.snMPP()
         file = open("ports", "w")
         self.port_list = {}
         for index in range(15):
             single_port = PORT+str(index+1)
-            port_status = self.snmp_handler.get(single_port)
-            file.write(str(single_port)+" "+str(port_status))
-            self.port_list[str(single_port)] = port_status
-            print(self.port_list)
+            # port_status = self.snmp_handler.get(single_port)
+            # file.write(str(single_port)+" "+str(port_status))
+            self.port_list[str(single_port)] = 1  # port_status
+        print(self.port_list)
         super().__init__(application, request, **kwargs)
 
     def get(self):
-        self.write("Hello, world")
+        loader = tornado.template.Loader("/home/dev/OGRC_internet_access_control/templates")
+        self.write(loader.load("switch.html").generate())
+        # self.write("Hello, world")
 
     def post(self, *args, **kwargs):
         print("oh man")
