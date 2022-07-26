9fa6f3cf8201fae9768d498902fff206db7ac558
AlexandreJSehnem <alexandre.jsehnem@gmail.com>
2022-07-26 11:17:24 -0300

    Added a division to the ports, to simulate multiple classes with a different set of ports for each

--- server.py
+++ server.py
@@ -13,13 +13,18 @@ class MainHandler(tornado.web.RequestHandler):
     def __init__(self, application, request: httputil.HTTPServerRequest, **kwargs) -> None:
         self.snmp_handler = easy_get_set.snMPP()
         file = open("ports", "w")
-        self.port_list = {}
-        for index in range(15):
+        self.port_list_1 = {}
+        self.port_list_2 = {}
+        for index in range(5):
             single_port = PORT+str(index+1)
             port_status = self.snmp_handler.get(single_port)[-1][1]
             # file.write(str(single_port)+" "+str(port_status))
-            self.port_list[str(single_port)] = [int(
-                port_status), index+1]  # port_status
+            self.port_list_1[str(single_port)] = [int(port_status), index+1]  # port_status
+        for index2 in range(5, 10):
+            single_port = PORT+str(index2+1)
+            port_status = self.snmp_handler.get(single_port)[-1][1]
+            # file.write(str(single_port)+" "+str(port_status))
+            self.port_list_2[str(single_port)] = [int(port_status), index2+1]  # port_status
         file.close()
         self.loader = tornado.template.Loader(
             "./templates")
@@ -31,22 +36,35 @@ class MainHandler(tornado.web.RequestHandler):
     def post(self, *args, **kwargs):
         user = self.get_body_argument("user", None)
         senha = self.get_body_argument("senha", None)
-        if not user and not senha:
-            for i in range(1, 16):
+        manage = self.get_body_argument("form", None)
+        if manage:
+            for i in range(1, 6):
                 status = self.get_body_argument(str(i), None)
                 if status:
                     if status == "Ligado":
                         port_oid = PORT + str(i)
                         print(port_oid)
-                        self.port_list[port_oid][0] = 0
+                        self.port_list_1[port_oid][0] = 0
                         self.snmp_handler.set(port_oid, 2)
                     elif status == "Desligado":
                         port_oid = PORT + str(i)
-                        self.port_list[port_oid][0] = 1
+                        self.port_list_1[port_oid][0] = 1
                         self.snmp_handler.set(port_oid, 1)
-            self.write(self.loader.load(
-                "manager.html").generate(ports=self.port_list))
+            for j in range(6, 11):
+                status = self.get_body_argument(str(j), None)
+                if status:
+                    if status == "Ligado":
+                        port_oid = PORT + str(j)
+                        print(port_oid)
+                        self.port_list_2[port_oid][0] = 0
+                        self.snmp_handler.set(port_oid, 2)
+                    elif status == "Desligado":
+                        port_oid = PORT + str(j)
+                        self.port_list_2[port_oid][0] = 1
+                        self.snmp_handler.set(port_oid, 1)
+            self.write(self.loader.load("manager.html").generate(ports=self.port_list_1, ports2=self.port_list_2))
             return
+
         file = open("auth", "r")
         count = 0
         for line in file:
@@ -58,10 +76,11 @@ class MainHandler(tornado.web.RequestHandler):
                 count += 1
                 continue
             else:
+                file.close()
                 self.write(self.loader.load("login.html").generate())
                 return
-        self.write(self.loader.load(
-            "manager.html").generate(ports=self.port_list))
+        file.close()
+        self.write(self.loader.load("manager.html").generate(ports=self.port_list_1, ports2=self.port_list_2))
 
 
 if __name__ == "__main__":
